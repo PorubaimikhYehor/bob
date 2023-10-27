26ec93fea00c89abccc8e28f8a0dd8de49a85123
PorubaimikhYehor <porubaimikh.yehor@gmail.com>
2023-10-28 01:40:18 +0300

    first

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,120 @@
+﻿using System.Text;
+
+namespace ClassLibrary1;
+public class Patiend
+{
+  private string _name = string.Empty;
+  public string GetName() => _name;
+  public bool SetName(string value, out string errorMessage)
+  {
+    errorMessage = string.Empty;
+    if (value == null)
+    {
+      errorMessage = "The name should not be empty";
+    }
+    else if (value.Length < 2)
+    {
+      errorMessage = "Patient name should contain at least two or more characters";
+    }
+    else
+    {
+      _name = value;
+    }
+    return string.IsNullOrEmpty(errorMessage);
+  }
+
+  private int _age = 0;
+  public int GetAge() => _age;
+  public bool SetAge(string value, out string errorMessage)
+  {
+    errorMessage = string.Empty;
+    int intValue;
+    if (int.TryParse(value, out intValue))
+    {
+      SetAge(intValue, out errorMessage);
+    }
+    else
+    {
+      errorMessage = "Patient age can be only number";
+    }
+    return string.IsNullOrEmpty(errorMessage);
+
+  }
+  public bool SetAge(int value, out string errorMessage)
+  {
+    errorMessage = string.Empty;
+    if (value < 0)
+    {
+      errorMessage = "Patient age can't be negative";
+    }
+    else if (value > 100)
+    {
+      errorMessage = "Patient age can't be greater than 100";
+    }
+    else
+    {
+      _age = value;
+    }
+    return string.IsNullOrEmpty(errorMessage);
+  }
+
+  private Gender _gender;
+  public Gender GetGender() => _gender;
+  public bool SetGender(string value, out string errorMessage)
+  {
+    StringBuilder sb = new StringBuilder();
+    Gender gender;
+    if (string.IsNullOrEmpty(value))
+    {
+      sb.Append("The gender should not be empty");
+    }
+    else if (!Enum.TryParse(value.ToUpper(), out gender))
+    {
+
+      sb.Append("Patient gender should be either ");
+      Array enumValues = Enum.GetValues(typeof(Gender));
+      foreach (Gender item in enumValues)
+      {
+        // Console.WriteLine(item);
+        sb.Append(item);
+        if (Array.IndexOf(enumValues, item) == enumValues.Length - 2)
+        {
+          sb.Append(" or ");
+        }
+        else if (Array.IndexOf(enumValues, item) != enumValues.Length - 1)
+        {
+          sb.Append(", ");
+        }
+      };
+    }
+    else
+    {
+      _gender = gender;
+    }
+    errorMessage = sb.ToString();
+    return string.IsNullOrEmpty(errorMessage);
+  }
+  private string _medicalHistory = string.Empty;
+  public string GetMedicalHistory() => _medicalHistory;
+  public void SetMedicalHistory(string value) => _medicalHistory = value;
+  private string _symptomCode = string.Empty;
+  public string GetSymptomCode() => _symptomCode;
+  public void SetSymptomCode(string value) => _symptomCode = value;
+
+  private string _prescription = string.Empty;
+  public string GetPrescription() => _prescription;
+  public void SetPrescription(string value) => _prescription = value;
+}
+
+public enum Gender
+{
+  MALE,
+  FEMALE,
+  OTHER
+}
+
+public class Symptom
+{
+  public required string Code { get; set; }
+  public required string Description { get; set; }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,81 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassLibrary1;
+
+Patiend patined = new Patiend();
+Console.WriteLine("Enter your (patient) details:");
+string errorMessage = string.Empty;
+
+Console.Write("Enter Patient Name: ");
+while (!patined.SetName(Console.ReadLine() ?? string.Empty, out errorMessage))
+{
+  Console.WriteLine(errorMessage);
+}
+
+Console.Write("Enter Patient Age: ");
+while (!patined.SetAge(Console.ReadLine() ?? string.Empty, out errorMessage))
+{
+  Console.WriteLine(errorMessage);
+}
+
+List<string> items = new List<string>();
+foreach (Gender item in Enum.GetValues(typeof(Gender))) items.Add(item.ToString());
+createHorisontalSelector(items, "Select the gender: ");
+
+Console.Write("Enter Medical History. Eg: Diabetes. Press Enter for None: ");
+patined.SetMedicalHistory(Console.ReadLine() ?? string.Empty);
+
+Console.WriteLine();
+Console.WriteLine($"Welcome, {patined.GetName()}, {patined.GetAge()}.");
+Console.WriteLine("Which of the following symptoms do you have:");
+createHorisontalSelector(new List<string> {"S1", "S2", "S3"}, string.Empty);
+
+static int createHorisontalSelector(List<string> items, string? title)
+{
+  if(title == null)
+  {
+    title = string.Empty;
+  }
+  Console.Write(title);
+  Console.CursorVisible = false;
+  int cursorPosLeft = title.Length;
+  int selectedIndex = 0;
+  for (; ; )
+  {
+    for (int i = 0; i < items.Count; i++)
+    {
+      if (i == selectedIndex)
+      {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.BackgroundColor = ConsoleColor.DarkBlue;
+      }
+      else
+      {
+        Console.ResetColor();
+      }
+      Console.Write(string.Format(" {0} ", items[i]));
+    }
+
+    Console.WriteLine();
+    var keyInfo = Console.ReadKey(intercept: true);
+    var cursorPos = Console.GetCursorPosition();
+    Console.SetCursorPosition(cursorPosLeft, cursorPos.Top - 1);
+    if (keyInfo.Key == ConsoleKey.LeftArrow && selectedIndex > 0)
+    {
+      --selectedIndex;
+    }
+    else if (keyInfo.Key == ConsoleKey.RightArrow && selectedIndex < items.Count - 1)
+    {
+      ++selectedIndex;
+    }
+    else if (keyInfo.Key == ConsoleKey.Enter)
+    {
+      Console.ResetColor();
+      Console.SetCursorPosition(cursorPosLeft, cursorPos.Top - 1);
+      Console.WriteLine(new string(' ', items.ConvertAll(i => i.Length).Sum() + items.Count * 2));
+      Console.SetCursorPosition(cursorPosLeft, cursorPos.Top - 1);
+      Console.WriteLine(items[selectedIndex]);
+      return selectedIndex;
+    }
+  }  
+}
+
